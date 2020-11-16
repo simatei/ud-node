c07bc4ff6d10a15ec1e851214693a5bfefacd1de
simatei <dr.robsimatei@gmail.com>
2020-11-16 19:34:06 +0300

    install nodemon -g

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 const log = console.log
 
 const getNotes = function () {
-    return log(chalk.green.bgRed.bold('Your notes..'))
+    return log(chalk.green.bgRed('Success.'))
 }
 
 
